706b9a722d3c069c1bbd999b19912a5068247555
Elvis Franks Condori Arias <massivestar7@gmail.com>
2021-04-18 14:21:30 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fragmentviewmodel/BlankFragment1.kt
@@ -0,0 +1,30 @@
+package com.example.fragmentviewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.fragmentviewmodel.databinding.FragmentBlank1Binding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment1 : Fragment() {
+
+    lateinit var binding: FragmentBlank1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_blank1,container,false)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/fragmentviewmodel/BlankFragment2.kt
@@ -0,0 +1,31 @@
+package com.example.fragmentviewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.fragmentviewmodel.databinding.FragmentBlank2Binding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment2 : Fragment() {
+
+    lateinit var binding: FragmentBlank2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_blank2,
+            container,false)
+
+        return binding.root
+    }
+
+}
